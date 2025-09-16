30147cf193e6e4d9397fa48a1f3118bd0c20d49e
niteshNextupgrad <nitesh.nextupgrad@gmail.com>
2025-09-16 21:59:38 +0530

    modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -36,7 +36,7 @@ const useWebSocket = (sessionId, role) => {
             }
 
             // Handle summary response
-            if (data.type === 'summary' && role === 'spectator') {
+            if (data.type === 'summary' && role === 'magician') {
               console.log("Summary Data received:", data);
               // console.log("Topics received:", data .topics);
               // Open new tab with the topic
